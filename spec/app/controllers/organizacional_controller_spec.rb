bad35d781e5cd4bdfa5145d1b5ff5e6e6bd0e608
aureliano <aureliano.franca@hotmail.com>
2013-10-25 19:12:19 -0200

    Testa rota para página de listagem de partidos.

--- spec/app/controllers/organizacional_controller_spec.rb
+++ spec/app/controllers/organizacional_controller_spec.rb
@@ -1,11 +1,14 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe "OrganizacionalController" do
-  before do
-    get "/"
-  end
 
-  it "returns hello world" do
-    last_response.body.should == "Hello World"
+  it "carrega página com lista de partido políticos" do
+    get '/organizacional/partidos'
+    
+    last_response.body.should include '<title>Res Publica: Partidos Políticos</title>'
+    last_response.body.should include '<form method="get" id="form_consulta" action="/organizacional/partidos" protect_from_csrf="true" accept-charset="UTF-8">'
   end
+  
 end
